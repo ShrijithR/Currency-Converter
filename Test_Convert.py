a7b1fac2a5f0d244636021cb9e42d68374e1dd6c
ShrijithR <72496419+ShrijithR@users.noreply.github.com>
2020-11-07 16:46:51 +0530

    Update Test_Convert.py

--- Test_Convert.py
+++ Test_Convert.py
@@ -1,5 +1,5 @@
 import unittest
-
+import time
 from CurrencyToWords import Main_Function_Call
 from src.support import Input_Validation
 from src.support import Attrs
@@ -10,11 +10,6 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(
             self.convert_amount.convert(), 
             "Rs. One Lakh Twenty Three Thousand Four Hundred and Fifty Six 78/100 ONLY"
-            )
-    def test_Correct_B(self):
-        self.validate = Input_Validation('90005.00')
-        self.assertTrue(
-            self.validate.validate()
             )
     def test_ToFloat(self):
         attr = Attrs('2340')
@@ -28,35 +23,43 @@ class TestConvert(unittest.TestCase):
             "Rs. Zero"
         )
     def test_InvalidCharacter(self):
-        self.validate = Input_Validation('sdd')
+        self.convert_amount = Main_Function_Call('sdd')
         self.assertEqual(
-            self.validate.validate(),
-            "Invalid input"
+            self.convert_amount.convert(),
+            "sdd contains invalid input character"
         )
     def test_NumberOfDots(self):
-        self.validate = Input_Validation('34.56.34')
+        self.convert_amount = Main_Function_Call('34.56.34')
         self.assertEqual(
-            self.validate.validate(),
-            "Invalid input"
+            self.convert_amount.convert(),
+            "34.56.34 contains invalid input character"
         )
     def test_DecimalLimit(self):
         self.convert_amount = Main_Function_Call('234.5436')
         self.assertEqual(
             self.convert_amount.convert(),
-            "Invalid input"
+            "234.5436 contains invalid input character"
         )
     def test_StartWithDigit(self):
         self.convert_amount = Main_Function_Call('.05')
         self.assertEqual(
             self.convert_amount.convert(),
-            "Invalid input"
+            ".05 contains invalid input character"
         )
     def test_EndWithDigit(self):
         self.convert_amount = Main_Function_Call('0.3a')
         self.assertEqual(
             self.convert_amount.convert(),
-            "Invalid input"
+            "0.3a contains invalid input character"
+        )
+    def test_Limit(self):
+        self.convert_amount = Main_Function_Call('9999789')
+        self.assertEqual(
+            self.convert_amount.convert(),
+            "9999789 is not in between 0 and 999999.99"
         )
     
 if __name__ == "__main__":
     unittest.main()
+    time.sleep(3)
+
